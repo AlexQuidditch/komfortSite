d80e6fdfb0607a2ad82a03b6d2505ca3d5c04b57
AlexQuidditch <alexquidditch@gmail.com>
2016-12-26 01:02:11 +0700

    termokraft in work

--- app/js/scroll.js
+++ app/js/scroll.js
@@ -1,16 +1,10 @@
 $(document).ready(function () {
-
-	var anchorTrigger = $('[data-roll]');
-
-	anchorTrigger.on('click', function (event) {
-		event.preventDefault();
-		var
-			$this = $(this),
-			target = $this.data('roll'),
-			anchor = $('[id="' + target + '"]');
-		anchor.velocity("scroll", {
-			easing: 'easeInOut',
-			duration: 1500
-		})
-	});
+    $('a[href^="#"]').click(function () {
+        var elementClick = $(this).attr("href"),
+            destination = $(elementClick).offset().top;
+        $('html,body').animate({
+            scrollTop: destination
+        }, 750);
+        return false;
+    });
 });
--- app/js/scrollUp.js
+++ app/js/scrollUp.js
@@ -1,37 +1,37 @@
 $(document).ready(function () {
 
-	var buttonUp = $('.button-up');
+    var buttonUp = $('.button-up');
 
-	$('body').append(
-		'<div class="button-up" style="z-index:1000;display: none;opacity: 0.5;width: 10%;height:100%; position: fixed; left: 0px; top: 0px; cursor: pointer; text-align: center; font-size:16px; line-height: 175vh; color: #45688E;"><i class="fa fa-arrow-circle-up fa-4x"></i>&nbsp;</div>'
-	);
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 700) {
-			buttonUp.fadeIn();
-		} else {
-			buttonUp.fadeOut();
-		}
-	});
-	buttonUp.click(function () {
-		$('body,html').velocity({
-			scrollTop: 0
-		}, 750);
-		return false;
-	});
-	buttonUp.hover(function () {
-		$(this).velocity({
-			'opacity': '0.9'
-		}).css({
-			'background-color': '#E1E7ED',
-			'color': '#45688E',
-		});
-	}, function () {
-		$(this).velocity({
-			'opacity': '0.4'
-		}).css({
-			'background': 'none',
-			'color': '#45688E',
-		});
-	});
+    $('body').append(
+        '<div class="button-up" style="z-index:1000;display: none;opacity: 0.5;width: 10%;height:100%; position: fixed; left: 0px; top: 0px; cursor: pointer; text-align: center; font-size:16px; line-height: 175vh; color: #45688E;"><i class="fa fa-arrow-circle-up fa-4x"></i>&nbsp;</div>'
+    );
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 700) {
+            buttonUp.fadeIn();
+        } else {
+            buttonUp.fadeOut();
+        }
+    });
+    buttonUp.click(function () {
+        $('body,html').velocity({
+            offset: "750px",
+        }, 750);
+        return false;
+    });
+    buttonUp.hover(function () {
+        $(this).velocity({
+            'opacity': '0.9'
+        }).css({
+            'background-color': '#E1E7ED',
+            'color': '#45688E',
+        });
+    }, function () {
+        $(this).velocity({
+            'opacity': '0.4'
+        }).css({
+            'background': 'none',
+            'color': '#45688E',
+        });
+    });
 
 })
